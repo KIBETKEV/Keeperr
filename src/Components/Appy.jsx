f7cb5233479747effc897e8134b5a183658f4cec
Kelvin Kibet <122841754+KIBETKEV@users.noreply.github.com>
2025-02-11 17:01:23 -0600

    Initial commit

---
+++ src/Components/Appy.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+
+function Appy() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+export default Appy;
---
+++ src/Components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Shopping</h1>
+      <p>Bread</p>
+    </div>
+  );
+}
+
+export default Note;
